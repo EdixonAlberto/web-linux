886a06573cdd598ad44f4ebcd47f3a09dd207fe0
Edixon Piña <edixonalbertto@gmail.com>
2021-05-13 23:48:27 -0400

    refactor in date and time dynamic and in styles of progress-bar

--- public/App.js
+++ public/App.js
@@ -8,8 +8,13 @@ new Vue({
   },
 
   methods: {
-    getDate() {
-      const d = this.date
+    initDateDynamic() {
+      setInterval(() => (this.date = new Date()), 1000 * 60)
+    }
+  },
+
+  computed: {
+    dateDynamic() {
       const weeks = [
         'Monday',
         'Tuesday',
@@ -33,9 +38,17 @@ new Vue({
         'November',
         'December'
       ]
-
+      const d = this.date
+      const hours = d.getHours() < 10 ? '0' + d.getHours() : d.getHours()
+      const minutes = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes()
       // prettier-ignore
-      return `${weeks[d.getDay() - 1]} ${months[d.getMonth()]} ${d.getMonth()} ${d.getDate()}, ${d.getHours()}:${d.getMinutes()}`
+      const time = `${weeks[d.getDay() - 1]} ${months[d.getMonth()]} ${d.getMonth()} ${d.getDate()}, ${hours}:${minutes}`
+
+      return { time, hours, minutes }
     }
+  },
+
+  created() {
+    this.initDateDynamic()
   }
 })
